d41e2bb47bfb429983f40e2eaa0846dc5c9ce9c7
igorTT <hirzhek@gmail.com>
2022-11-26 13:46:26 +0300

    added secrets

--- data/database.js
+++ data/database.js
@@ -4,8 +4,9 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
+const dbPort = process.env.MONGODB_PORT;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb://${dbUser}:${dbPassword}@${clusterAddress}:${dbPort}${dbName}`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
