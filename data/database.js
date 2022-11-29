3197e1291109a3dbba709909b0e30564f156cc97
igorTT <hirzhek@gmail.com>
2022-11-29 21:25:31 +0300

    added services

--- data/database.js
+++ data/database.js
@@ -3,13 +3,14 @@ import * as dotEnv from 'dotenv';
 
 dotEnv.config();
 
+const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 const dbPort = process.env.MONGODB_PORT;
 
-const uri = `mongodb://${dbUser}:${dbPassword}@${clusterAddress}:${dbPort}/${dbName}`;
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}:${dbPort}/${dbName}`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
