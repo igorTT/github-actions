8d2263765ba1118026cdcdb688c2c597e7ec40d5
igorTT <hirzhek@gmail.com>
2022-11-29 21:31:50 +0300

    switched to the services

--- data/database.js
+++ data/database.js
@@ -8,7 +8,6 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
-// const dbPort = process.env.MONGODB_PORT;
 
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
